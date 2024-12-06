06af144d11b73cbc5c17efc0ce97fce5a90bab71
maltemosbach <maltemosbach@gmail.com>
2024-12-06 17:13:37 +0100

    WIP: Add eval functionalities.

--- src/sold/eval_sold.py
+++ src/sold/eval_sold.py
@@ -1,13 +1,14 @@
 from collections import defaultdict
 import hydra
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import DictConfig
 from sold.train_sold import SOLDModule
 from sold.utils.training import set_seed
 import os
+from tqdm import tqdm
 import torch
 from torchvision.io import write_video
 from torchvision import transforms
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 os.environ["HYDRA_FULL_ERROR"] = "1"
 
@@ -30,27 +31,37 @@ def play_episode(sold: SOLDModule, mode: str = "eval") -> Dict[str, Any]:
     return episode
 
 
+def get_checkpoint_files(checkpoint_path: str) -> List[str]:
+    if os.path.isfile(checkpoint_path):
+        return [checkpoint_path] if checkpoint_path.endswith('.ckpt') else []
+    elif os.path.isdir(checkpoint_path):
+        return [os.path.join(checkpoint_path, file) for file in os.listdir(checkpoint_path) if file.endswith('.ckpt')]
+    else:
+        raise ValueError(f"The path '{checkpoint_path}' is neither a valid file nor directory.")
+
+
 @hydra.main(config_path="./configs", config_name="eval_sold")
 def evaluate(cfg: DictConfig):
     set_seed(cfg.seed)
-    env = hydra.utils.instantiate(cfg.env)
     output_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
+    checkpoint_files = get_checkpoint_files(cfg.checkpoint_path)
 
-    sold = SOLDModule.load_from_checkpoint(cfg.checkpoint, env=env)
+    for checkpoint in tqdm(checkpoint_files, disable=len(checkpoint_files) == 1, desc="Evaluating checkpoints"):
+        env = hydra.utils.instantiate(cfg.env)
+        sold = SOLDModule.load_from_checkpoint(checkpoint, env=env)
 
-    for episode_index in range(cfg.eval_episodes):
-        episode = play_episode(sold, mode="eval")
-        print("episode.keys():", episode.keys())
-
-        torch.stack(episode["obs"])
-        os.makedirs(os.path.join(output_dir, "videos"), exist_ok=True)
-        write_video(os.path.join(output_dir, f"videos/episode_obs_{episode_index}.mp4"),
-                    (torch.stack(episode["obs"]).permute(0, 2, 3, 1) * 255).to(torch.uint8), fps=10)
-        write_video(os.path.join(output_dir, f"videos/episode_high_res_{episode_index}.mp4"),
-                    (torch.stack(episode["high_res"]).permute(0, 2, 3, 1) * 255).to(torch.uint8), fps=10)
-
-    print("done")
-    input()
+        # Log behavior videos.
+        video_dir = os.path.join(output_dir, "videos")
+        os.makedirs(video_dir, exist_ok=True)
+        for episode_index in range(cfg.eval_episodes):
+            checkpoint_filename = os.path.splitext(os.path.basename(checkpoint))[0]
+            checkpoint_video_dir = os.path.join(video_dir, checkpoint_filename)
+            os.makedirs(checkpoint_video_dir, exist_ok=True)
+            episode = play_episode(sold, mode="eval")
+            write_video(os.path.join(checkpoint_video_dir, f"episode_obs_{episode_index}.mp4"),
+                        (torch.stack(episode["obs"]).permute(0, 2, 3, 1) * 255).to(torch.uint8), fps=10)
+            write_video(os.path.join(checkpoint_video_dir, f"episode_high_res_{episode_index}.mp4"),
+                        (torch.stack(episode["high_res"]).permute(0, 2, 3, 1) * 255).to(torch.uint8), fps=10)
 
 
 if __name__ == "__main__":
