b21ecab40faef57511b72cd768e7618487d82bc8
maltemosbach <maltemosbach@gmail.com>
2025-01-03 14:53:12 +0100

    Clean up environment wrappers.

--- sold/envs/wrappers/to_tensor.py
+++ sold/envs/wrappers/to_tensor.py
@@ -1,13 +1,12 @@
 import gym
 import torch
-from torchvision import transforms
 from typing import Tuple
 
 
 class ToTensor(gym.Wrapper):
     def step(self, action: torch.Tensor) -> Tuple[torch.Tensor, float, bool, dict]:
         obs, reward, done, info = self.env.step(action.detach().cpu().numpy())
-        return transforms.ToTensor()(obs.copy()), reward, done, info
+        return torch.from_numpy(obs).float(), reward, done, info
 
     def reset(self) -> torch.Tensor:
-        return transforms.ToTensor()(self.env.reset().copy())
+        return torch.from_numpy(self.env.reset()).float()
